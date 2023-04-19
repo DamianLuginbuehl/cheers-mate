008aa12ade4b65e79edd635266677cbb454e7346
Damian Luginbühl <private@damian-luginbuehl.ch>
2023-04-19 16:44:42 +0200

    02/04

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+let loadPosts = function() {
+    $(".postcard.page-1").addClass("visible");
+    $(".pagination-button.page-1").addClass("active");
+}
+
+let changePage = function(page) {
+    $(".postcard").removeClass("visible");
+    $(".pagination-button").removeClass("active");
+    $(".pagination-button.page-"+page).addClass("active");
+    $(".postcard.page-"+page).addClass("visible");
+}
