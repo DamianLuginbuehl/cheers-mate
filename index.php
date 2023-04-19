e2d9c20a6512b9705ac271405cbf58b14b6afca6
Damian Luginbühl <private@damian-luginbuehl.ch>
2023-04-19 15:18:12 +0200

    02/03

--- index.php
+++ index.php
@@ -17,13 +17,16 @@
 
                 <div class="post-container">
                     <div class="post-image">
+
                         <div class="post-info">
                             <div class="post-meta">
                                 <time datetime="<?php the_time('d.m.Y, H:M') ?>"><?php the_time('d.m.Y, H:m') ?></time>
+                                <ul class="categories">
                                 <?php $categories = get_the_category(); // getting all category objects of the current post
                                 foreach ($categories as $category) {
-                                    echo '<a class="first-category" href="' . get_category_link($category->term_id) . '">' . $category->name . '</a> ';
+                                    echo '<li><a class="category-white" href="' . get_category_link($category->term_id) . '">' . $category->name . '</a></li>';
                                 } ?>
+                                </ul>
                             </div>
                             <h1 class="post-title">
                                 <?php the_title() ?>
